a4abb354a4d053b08398bd58fe4f0e29cce89a51
Alma Flores <flores.almalee@gmail.com>
2020-08-29 23:33:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/[a-zA-z]/).length < phone.length ? false : true 
+  phone.scan(/[a-zA-z]/).length < phone.length ? true : false
 end
